7344e0c1248d9e640c06e65adec9875c9473c161
ostasha taras <tonyjoss1990@gmail.com>
2016-10-11 23:17:14 +0300

    done to realiz

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 			data: $(this).serialize()
 		}).done(function() {
 			// alert('Thanks for your email. \n We well contact you as soon as possible');
-			toastr.success('Thanks for your email. \n We well contact you as soon as possible');
+			toastr.success('Thanks for your email. \n We will contact you as soon as possible');
 			$("#form-site").get(0).reset();
 		});
 		return false;
